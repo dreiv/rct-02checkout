5642527c58ebb39f5df5e7d9bef76d31937508fc
dreiv <dreivoicu@gmail.com>
2018-06-13 19:39:15 +0300

    back into it

---
+++ src/components/CheckoutArea/Checkout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Checkout.css'
+
+const propTypes = {}
+
+const defaultProps = {}
+
+export default function Checkout(props) {
+	return <React.Fragment />
+}
+
+Checkout.propTypes = propTypes
+Checkout.defaultProps = defaultProps
